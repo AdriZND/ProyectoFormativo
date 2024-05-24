f7f982d7b260d82e63201bd3fca114bcb70c6251
AdriZND <adrimazamar@gmail.com>
2024-05-24 14:23:50 +0200

    Cambiada configuración docker para servir static desde contenedor del backend en producción

--- Backend/server.js
+++ Backend/server.js
@@ -34,7 +34,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // Serve static files from the 'dist' directory
-
+app.use("/", express.static(path.join(__dirname, "dist")))
 //Provide static
 app.use("/app/assets", express.static(path.join(__dirname, "app", "assets")))
 
